ab9267d1821622f1ed9c604c2954985cb8046ed5
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-01-03 18:39:39 +0100

    add responsive music section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 export default {
 	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		colors: {
+			...colors,
 			"color-main": "#2A2A48",
 			"color-main-dark": "#1E1E1E",
 			"color-main-light": "#E5E5E5",
