be789fb3a085a8e4acdfe7401c71cb3298e9be47
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-01-08 00:01:33 +0100

    add three js animation

--- src/components/Blog/Temporary.tsx
+++ src/components/Blog/Temporary.tsx
@@ -1,42 +1,60 @@
 import React, { useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256)
+  const g = Math.floor(Math.random() * 256)
+  const b = Math.floor(Math.random() * 256)
+  return `rgb(${r},${g},${b})`
+}
 
 function Sphere(props: any) {
   const meshRef = useRef()
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
+
+  const [color, setColor] = useState(getRandomColor())
+
   useFrame((state, delta) => (meshRef.current.rotation.x += delta))
 
   return (
     <mesh
       {...props}
       ref={meshRef}
-      scale={active ? 0.25 : 0.5}
-      onClick={event => setActive(!active)}
-      onPointerOver={event => setHover(true)}
-      onPointerOut={event => setHover(false)}
+      scale={0.5}
+      onPointerOver={e => setColor(getRandomColor())}
     >
       <torusKnotGeometry args={[10, 3, 100, 16]} />
-      <meshStandardMaterial color={hovered ? 'aqua' : 'black'} />
+      <meshStandardMaterial color={color} roughness={0.2} />
     </mesh>
   )
 }
 
 export function Temporary() {
+  const [isLightBottom, setIsLightBottom] = useState(true)
+
   return (
-    <Canvas>
-      <ambientLight intensity={Math.PI / 2} />
-      <spotLight
-        position={[10, 10, 10]}
-        angle={0.15}
-        penumbra={1}
-        decay={0}
-        intensity={Math.PI}
-      />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Sphere position={[0, 0, -10]} />
-    </Canvas>
+    <>
+      <div>
+        <Canvas className='bg-black rounded-full' style={{ height: 500 }}>
+          <pointLight
+            position={[0, -10, 0]}
+            decay={0}
+            intensity={isLightBottom ? 3 : 1}
+          />
+
+          <Sphere position={[0, 0, -50]} />
+          <Sphere position={[100, 100, -50]} />
+          <Sphere position={[100, -200, -50]} />
+          <OrbitControls makeDefault />
+        </Canvas>
+      </div>
+
+      <button
+        className='mt-3'
+        onClick={() => setIsLightBottom(state => !state)}
+      >
+        {isLightBottom ? '💡' : '👜'}
+      </button>
+    </>
   )
 }
