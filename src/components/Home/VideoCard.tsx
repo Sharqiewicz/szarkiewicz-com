39da74a15866fe845eb3a3a97a20698df6bc917c
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-01-18 19:00:37 +0100

    fix card width

--- src/components/Home/VideoCard.tsx
+++ src/components/Home/VideoCard.tsx
@@ -1,6 +1,6 @@
 export const VideoCard = ({ itemInfo }: { itemInfo: VideoInfoType }) => (
   <article className='relative mb-20 ml-[10vw]'>
-    <div className=' w-[60vw] lg:w-[60vw] relative z-30 rounded'>
+    <div className=' w-[60vw] lg:w-[60vw] max-w-[800px] relative z-30 rounded'>
       <a
         key={itemInfo.id}
         className='group relative overflow-hidden cursor-pointer z-40'
@@ -13,7 +13,7 @@ export const VideoCard = ({ itemInfo }: { itemInfo: VideoInfoType }) => (
         ></img>
       </a>
     </div>
-    <div className='absolute bg-black w-full h-full -top-5 -right-5 z-20 rounded'></div>
+    <div className='absolute bg-black w-full h-full max-w-[600px] -top-5 -right-5 z-20 rounded'></div>
     <span className='absolute top-5 right-5 z-50 rounded text-xl md:text-4xl'>
       {itemInfo.lang}
     </span>
